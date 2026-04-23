45fb00c217afcd4a42c047f2c298ca5b20462c68
teekayyj-dotcom <letuankiet0012@gmail.com>
2026-04-23 11:07:45 +0700

    Rename the bucketname inside the terraform

--- infrastructure/environments/dev/backend.tf
+++ infrastructure/environments/dev/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-tf-state-dev"
+    bucket         = "kapt-terraform-states"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
+    dynamodb_table = "kapt-terraform-locks"
   }
 }
